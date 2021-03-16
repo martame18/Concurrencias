e98caf508454ef7930a473f20226e3b268f1bcef
Marta <martamaleno.185@hotmail.com>
2021-03-16 21:09:26 +0100

    Ejercicios del 06-03-2021

---
+++ Entregas c/Ejercicios_2.c
@@ -0,0 +1,26 @@
+/*
+* Hecho por Marta Maleno Escudero
+*
+* Ejercicios para hacer en casa del 06-03-2021
+*/
+
+// Ejercicio 1
+
+/*
+* Dado: 
+* < int t[30]; int *p; >
+* 
+* Podríamos hacer < p = t;> porque estableceríamos que el puntero apuntase al primer valor del array t.
+* Pero no sería correcto hacer < t = p;> porque p no es un entero, como los valores que han de almacenarse
+* en t, es un puntero a un entero y produciría un error.
+*/
+
+
+// Ejercicio 2
+
+    // Ni idea :(
+
+// Ejercicio 3
+
+/* Si ptr es un puntero a un entero &ptr es un entero que almacena el lugar al que apunta el puntero. */
+
