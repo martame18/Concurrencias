56b3e31c73b8b88bbb59aa9533be57954f843a6d
Marta <martamaleno.185@hotmail.com>
2021-03-18 19:26:43 +0100

    Ejercicios 1-8 bien, corregir 9

--- Entregas c/Ejercicios_2.c
+++ Entregas c/Ejercicios_2.c
@@ -18,9 +18,59 @@
 
 // Ejercicio 2
 
-    // Ni idea :(
+/* No tiene sentido que una instrucción de tipo &var aparezca a la izquierda de una asignación porque es un
+operador que almacena la dirección de una variable ya existente y esto no se puede alterar. */
+
 
 // Ejercicio 3
 
 /* Si ptr es un puntero a un entero &ptr es un entero que almacena el lugar al que apunta el puntero. */
 
+
+// Ejercicio 4
+
+/* El código imprime: P */
+
+
+// Ejercicio 5
+
+/* El código imprime: 1.0 */
+
+
+// Ejercicio 6
+
+char *str = "Empezamos una y otra vez";
+char *ptr;
+char a[28];
+ptr = str;
+a = "Empezamos una y otra vez";
+str = &a;
+
+
+// Ejercicio 7
+
+void cambiaSigno (int *ptr){
+    ptr = -ptr;
+}
+
+
+// Ejercicio 8
+
+int check(char *ptr, char *c){
+    if(ptr == &c){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+
+// Ejercicio 9
+
+void elimina(char c, char *cadena){
+    while (cadena!= 0){
+        if (*cadena == 0){
+            *cadena += 1;
+        }
+    }
+}
