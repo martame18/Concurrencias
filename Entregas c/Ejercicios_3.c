c877268b13df98a0fd479ba4d338a1a4ba886073
Marta <martamaleno.185@hotmail.com>
2021-03-22 16:26:08 +0100

    Ejercicios 3

--- Entregas c/Ejercicios_3.c
+++ Entregas c/Ejercicios_3.c
@@ -13,11 +13,33 @@ struct Persona{
 }
 
 void inicializa(struct Persona *p){
-    // ESTO HAY QUE COMPROBARLO PORQUE ESTA REGULERO ------------------------------------------------------
-
-    Persona pers;
     printf("Introduce un nombre:\n");
-    scanf("%c", pers.nombre);
+    scanf("%c", p.nombre);
     printf("\nIntroduce una edad:\n");
-    scanf("d", pers.edad);
+    scanf("d", p.edad);
+}
+
+void escribe(struct Persona p){
+    printf("Nombre: %c, Edad: %c\n", p.nombre, p.edad);
+}
+
+void inicializaLista(struct Persona *lista, int m, int* n){
+    printf("Introduce menos de %d personas:\n", n);
+    while (m>=n && lista[n-1].edad!=0){
+        inicializa(lista[n]);
+        n++;
+    }
+}
+
+void escribeLista(struct Persona *lista, int m){
+    for (int i=0; i<m; i++){
+        escribe(lista[i]);
+    }
+}
+
+int main(){
+    Persona[10] lista, *lista;
+    int cant=0, *cant;
+    inicializaLista(*lista, 10, *cant);
+    escribeLista(*lista, 10);
 }
