70a99addd7eb95244cebd6d723725f98483bae84
Marta <martamaleno.185@hotmail.com>
2021-05-20 14:33:26 +0200

    Hecho por la profesora

--- Ejercicios_clase/EjerBarca/Barca.java
+++ Ejercicios_clase/EjerBarca/Barca.java
@@ -4,8 +4,13 @@ public class Barca extends Thread{
 	
 	private static final int C = 4;
 	
-
+	private int nAnd = 0;
+	private int nIph = 0;
 	
+	
+	private boolean libreAnd = true;  //true sii puede subirse a la barca de forma segura
+	private boolean libreIph = true;
+	private boolean finViaje = false;
 
 	/**
 	 * Un estudiante con m�vil android llama a este m�todo 
@@ -17,8 +22,29 @@ public class Barca extends Thread{
 	 */
 	
 	public synchronized void android(int id) throws InterruptedException{
-		
-		
+		while (!libreAnd) wait();
+		nAnd++;
+		System.out.println("Sube android"+id);
+		if(nAnd == 2 && nIph == 1) libreAnd = false;
+		else if(nAnd == 1 && nIph == 2) libreIph = false;
+		else if(nAnd == 3) libreIph = false;
+		if (nAnd+nIph < 4) {
+			while(!finViaje) wait();
+		} else {
+			libreAnd = false;
+			libreIph = false;
+			finViaje = true;
+			notifyAll();
+		}
+		// se baja de la barca
+		nAnd--;
+		System.out.println("Baja android" +id);
+		if (nAnd+nIph == 0) {
+			libreAnd = true;
+			libreIph = true;
+			finViaje = false;
+			notifyAll();
+		}
 	}
 	
 	/**
